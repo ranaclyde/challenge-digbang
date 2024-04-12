12610cad792f4e4d6d5504aca1dad8971dfd3893
Matías E Sanhueza <manoloesanhueza@gmail.com>
2024-04-12 11:17:33 -0300

    Create the base component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
-import { Text } from '@chakra-ui/react'
+import { Box, Center, Text } from '@chakra-ui/react'
 
 function App() {
-  return <Text>Hello</Text>
+  return (
+    <Center h="100vh" bgColor="#084F85">
+      <Box
+        w="350px"
+        h="400px"
+        bgGradient="linear(to-b, #003B67, transparent)"
+        color="white"
+        p={5}
+      >
+        <Text
+          color="white"
+          textAlign="center"
+          fontWeight="bold"
+          fontSize="large"
+        >
+          Simulá tu crédito
+        </Text>
+      </Box>
+    </Center>
+  )
 }
 
 export default App
