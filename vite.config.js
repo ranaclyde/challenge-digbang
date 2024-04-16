da48e73800e27483147e83a600dd60e1d608403a
Matías E Sanhueza <manoloesanhueza@gmail.com>
2024-04-16 16:04:30 -0300

    Add lint to github workflow

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/test/setupTest.js',
+    setupFiles: './src/test/setupTest.js'
   }
 })
