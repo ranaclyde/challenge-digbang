6962c9cbcf4c17c3655bb66657a5d715e2755659
Matías E Sanhueza <manoloesanhueza@gmail.com>
2024-04-16 11:27:50 -0300

    Install and configure vitest

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://ranaclyde.github.io/challenge-digbang/'
+  base: 'https://ranaclyde.github.io/challenge-digbang/',
+  test: {
+    environment: 'jsdom',
+    globals: true
+  }
 })
